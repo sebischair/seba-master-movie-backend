7f384103a71ce2d0c36859bb56e091307b4d05bb
Philipp Landler <Philipp.Landler@tum.de>
2021-01-07 10:51:08 +0100

    changed movie datamodel and rating function

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,98 +1,133 @@
 "use strict";
 
-const MovieModel = require('../models/movie');
-
+const MovieModel = require("../models/movie");
 
 const create = async (req, res) => {
-    if (Object.keys(req.body).length === 0) return res.status(400).json({
-        error: 'Bad Request',
-        message: 'The request body is empty'
-    });
+    if (Object.keys(req.body).length === 0)
+        return res.status(400).json({
+            error: "Bad Request",
+            message: "The request body is empty",
+        });
 
     try {
-      let movie = await MovieModel.create(req.body);
+        let movie = await MovieModel.create(req.body);
 
-      return res.status(201).json(movie)
-    } catch(err) {
-      return res.status(500).json({
-        error: 'Internal server error',
-        message: err.message
-      });
+        return res.status(201).json(movie);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: "Internal server error",
+            message: err.message,
+        });
     }
 };
 
 const read = async (req, res) => {
-  try {
-    let movie = await MovieModel.findById(req.params.id).exec();
+    try {
+        let movie = await MovieModel.findById(req.params.id).exec();
 
-    if (!movie) return res.status(404).json({
-      error: 'Not Found',
-      message: `Movie not found`
-    });
+        if (!movie)
+            return res.status(404).json({
+                error: "Not Found",
+                message: `Movie not found`,
+            });
 
-    return res.status(200).json(movie)
-  } catch(err) {
-    return res.status(500).json({
-      error: 'Internal Server Error',
-      message: err.message
-    });
-  }
+        return res.status(200).json(movie);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: "Internal Server Error",
+            message: err.message,
+        });
+    }
 };
 
 const update = async (req, res) => {
     if (Object.keys(req.body).length === 0) {
         return res.status(400).json({
-            error: 'Bad Request',
-            message: 'The request body is empty'
+            error: "Bad Request",
+            message: "The request body is empty",
         });
     }
 
     try {
-      let movie = await MovieModel.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-      }).exec();
+        let movie = await MovieModel.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        ).exec();
 
-      return res.status(200).json(movie);
-    } catch(err) {
-      return res.status(500).json({
-        error: 'Internal server error',
-        message: err.message
-      });
+        return res.status(200).json(movie);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: "Internal server error",
+            message: err.message,
+        });
     }
 };
 
 const remove = async (req, res) => {
-  try {
-    await MovieModel.findByIdAndRemove(req.params.id).exec();
+    try {
+        await MovieModel.findByIdAndRemove(req.params.id).exec();
 
-    return res.status(200).json({message: `Movie with id${req.params.id} was deleted`});
-  } catch(err) {
-    return res.status(500).json({
-      error: 'Internal server error',
-      message: err.message
-    });
-  }
+        return res
+            .status(200)
+            .json({ message: `Movie with id${req.params.id} was deleted` });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: "Internal server error",
+            message: err.message,
+        });
+    }
 };
 
-const list  = async (req, res) => {
-  try {
-    let movies = await MovieModel.find({}).exec();
+const list = async (req, res) => {
+    try {
+        let movies = await MovieModel.find({}).exec();
 
-    return res.status(200).json(movies);
-  } catch(err) {
-    return res.status(500).json({
-      error: 'Internal server error',
-      message: err.message
-    });
-  }
+        return res.status(200).json(movies);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: "Internal server error",
+            message: err.message,
+        });
+    }
 };
 
+const rate = async (req, res) => {
+    try {
+        let votedMovieId = req.params.id;
+
+        let ratingObject = {
+            userId: req.userId,
+            rating: req.body.rating,
+        };
+
+        await MovieModel.findByIdAndUpdate(votedMovieId, {
+            $push: { audienceRatings: ratingObject },
+        });
+
+        return res.status(200).json({});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: "Internal server error",
+            message: err.message,
+        });
+    }
+};
 
 module.exports = {
     create,
     read,
     update,
     remove,
-    list
+    list,
+    rate,
 };
--- src/models/movie.js
+++ src/models/movie.js
@@ -1,32 +1,79 @@
 "use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+// this is that the later created virtuals are included in the json send to the user
+const opts = { toJSON: { virtuals: true } };
 
-// Define the movie schema
-const MovieSchema  = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    synopsis: String,
-    runtime: Number,
-    mpaa_rating: String,
-    year: {
+// Define schema for ratings
+const RatingSchema = new mongoose.Schema({
+    // user who gave the rating
+    userId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    // rating of user
+    rating: {
         type: Number,
-        required: true
+        min: 1,
+        max: 5,
     },
-    posters: {
-            thumbnail: String,
-            profile: String,
-            detailed: String,
-            original: String
-        }
 });
 
-MovieSchema.set('versionKey', false);
-MovieSchema.set('timestamps', true);
+// Define schema for actors for one movie
+const ActorSchema = new mongoose.Schema({
+    // name of actor
+    name: String,
+    // played roles
+    characters: [String],
+});
 
+// Define the movie schema
+const MovieSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        synopsis: String,
+        runtime: Number,
+        mpaa_rating: String,
+        theaterRelease: Date,
+        blurayRelease: Date,
+        criticsRating: {
+            type: Number,
+            min: 1,
+            max: 5,
+        },
+        audienceRatings: [RatingSchema],
+        actors: [ActorSchema],
+        thumbnail: String,
+    },
+    opts
+);
+// opts are added to MovieSchema since later we want to add calculated fields to the schema
+// and we want that these calculated fields are included in the json object of the entry
+
+RatingSchema.set("versionKey", false);
+ActorSchema.set("versionKey", false);
+MovieSchema.set("versionKey", false);
+MovieSchema.set("timestamps", true);
+
+// Virtuals are not stored in the db, they are calcualted on call
+MovieSchema.virtual("year").get(function () {
+    if (this.theaterRelease) {
+        return this.theaterRelease.getFullYear();
+    } else {
+        return "No Release Year";
+    }
+});
+MovieSchema.virtual("avgAudienceRating").get(function () {
+    let avgRating = 0;
+    let ratings = 0;
+    this.audienceRatings.map((rating) => {
+        avgRating += rating.rating;
+        ratings++;
+    });
+    avgRating = avgRating / ratings;
+    return avgRating;
+});
 
 // Export the Movie model
-module.exports = mongoose.model('Movie', MovieSchema);
+module.exports = mongoose.model("Movie", MovieSchema);
