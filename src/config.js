b04ff695f41299ae39460d0587327b0ebae7a465
Philipp Landler <Philipp.Landler@tum.de>
2021-04-09 11:04:50 +0200

    change port for compatibility with frontend

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 "use strict";
 
 // Configuration variables
-const port      = process.env.PORT        || '3000';
-const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost:27017/moviedb';
-const JwtSecret = process.env.JWT_SECRET  || 'very secret secret';
-
+const port = process.env.PORT || "4000";
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/moviedb";
+const JwtSecret = process.env.JWT_SECRET || "very secret secret";
 
 module.exports = {
     port,
