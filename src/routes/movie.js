4fcbe82da20f54e43b15216eab69063f423cc75e
Philipp Landler <Philipp.Landler@tum.de>
2021-04-09 11:16:51 +0200

    comment and clean up routes

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -12,21 +12,21 @@ router.post(
     middlewares.checkAuthentication,
     middlewares.checkIsAdmin,
     MovieController.create
-); // Create a new movie
+); // Create a new movie, needs logged in user with the admin role
 router.get("/:id", MovieController.read); // Read a movie by Id
 router.put(
     "/:id",
     middlewares.checkAuthentication,
     middlewares.checkIsAdmin,
     MovieController.update
-); // Update a movie by Id
+); // Update a movie by Id, needs logged in user with the admin role
 router.delete(
     "/:id",
     middlewares.checkAuthentication,
     middlewares.checkIsAdmin,
     MovieController.remove
-); // Delete a movie by Id
+); // Delete a movie by Id, needs logged in user with the admin role
 router.get("/rate/:id", MovieController.getRate); // Get the audience rating of this movie by Id
-router.put("/rate/:id", middlewares.checkAuthentication, MovieController.rate); // Rate movie with Id
+router.put("/rate/:id", middlewares.checkAuthentication, MovieController.rate); // Rate movie with Id. needs a logged in user
 
 module.exports = router;
